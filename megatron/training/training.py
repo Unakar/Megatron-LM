c9da3d6793b87d0049ad62648023aa6223fca6dd
unakar <xqs2002@mail.ustc.edu.cn>
2025-11-07 07:42:20 +0000

    fix import

--- megatron/training/training.py
+++ megatron/training/training.py
@@ -76,6 +76,7 @@ from megatron.core.distributed import finalize_model_grads
 from megatron.core.enums import ModelType
 from megatron.core.optimizer import get_megatron_optimizer, OptimizerConfig
 from megatron.core.optimizer.muon import get_megatron_muon_optimizer
+from megatron.core.optimizer.spectral_ball_optimizer import get_megatron_spectral_ball_optimizer
 from megatron.core.rerun_state_machine import (
     get_rerun_state_machine,
     destroy_rerun_state_machine,
@@ -1199,7 +1200,27 @@ def setup_model_and_optimizer(
     config = OptimizerConfig(**kwargs)
     config.timers = timers
 
-    if 'muon' not in config.optimizer:
+    if 'muon' in config.optimizer:
+        optimizer = get_megatron_muon_optimizer(
+            config,
+            model,
+            no_wd_decay_cond,
+            scale_lr_cond,
+            lr_mult,
+            use_gloo_process_groups=args.enable_gloo_process_groups,
+            layer_wise_distributed_optimizer='dist' in config.optimizer,
+        )
+    elif 'spectral_ball' in config.optimizer:
+        optimizer = get_megatron_spectral_ball_optimizer(
+            config,
+            model,
+            no_wd_decay_cond,
+            scale_lr_cond,
+            lr_mult,
+            use_gloo_process_groups=args.enable_gloo_process_groups,
+            layer_wise_distributed_optimizer='dist' in config.optimizer,
+        )
+    else:
         optimizer = get_megatron_optimizer(
             config,
             model,
@@ -1211,16 +1232,6 @@ def setup_model_and_optimizer(
             #  to avoid embeddings from shrinking to zero as recommended in https://arxiv.org/abs/2312.16903
             default_skip_embedding_weight_decay=args.embedding_init_method_std is not None,
             dump_param_to_param_group_map=args.dump_param_to_param_group_map,
-        )
-    else:
-        optimizer = get_megatron_muon_optimizer(
-            config,
-            model,
-            no_wd_decay_cond,
-            scale_lr_cond,
-            lr_mult,
-            use_gloo_process_groups=args.enable_gloo_process_groups,
-            layer_wise_distributed_optimizer='dist' in config.optimizer,
         )
 
     opt_param_scheduler = get_optimizer_param_scheduler(optimizer)
