a08d21ed4e62813b7ded306a03202555363a770b
unakar <xqs2002@mail.ustc.edu.cn>
2025-11-19 05:04:51 +0000

    回退到muon的lr scaler

--- emerging_optimizers/orthogonalized_optimizers/spectral_ball_utils.py
+++ emerging_optimizers/orthogonalized_optimizers/spectral_ball_utils.py
@@ -296,7 +296,7 @@ def get_spectral_ball_scale_factor(size_out: int, size_in: int, mode: str = "spe
     if mode == "shape_scaling":
         return max(1, size_out / size_in) ** 0.5
     elif mode == "align_adamw_rms":
-        return 0.2 * ( size_out/size_in * max(size_out, size_in)) ** 0.5
+        return 0.2 * max(size_out, size_in) ** 0.5
     elif mode == "unit_rms_norm":
         return (size_out / size_in) ** 0.5
     else:
