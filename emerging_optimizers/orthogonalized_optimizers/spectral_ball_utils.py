d3c28053b6c4aa389bbe86df1e0207038ee25cb6
unakar <xqs2002@mail.ustc.edu.cn>
2025-11-26 07:46:45 +0000

    这才是spball的align rms scaler!

--- emerging_optimizers/orthogonalized_optimizers/spectral_ball_utils.py
+++ emerging_optimizers/orthogonalized_optimizers/spectral_ball_utils.py
@@ -344,7 +344,7 @@ def get_spectral_ball_scale_factor(size_out: int, size_in: int, mode: str = "spe
     if mode == "shape_scaling":
         return max(1, size_out / size_in) ** 0.5
     elif mode == "align_adamw_rms":
-        return 0.2 * max(size_out, size_in) ** 0.5
+        return 0.2 * ( max(size_out, size_in) * size_in / size_out )** 0.5
     elif mode == "spectral_mup":
         return (size_out / size_in) ** 0.5
     else:
